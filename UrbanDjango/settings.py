a324efd87664e7ec8c52f9969fd43ff6f4cd8771
SergeevSS55 <yourhonor55@mail.ru>
2024-11-27 12:27:41 +0300

    Update settings.py

--- UrbanDjango/settings.py
+++ UrbanDjango/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'task2',
-    'task3',
+    # 'task2',
+    # 'task3',
     'task4',
 ]
 
